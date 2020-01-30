f93386158b36559c2c74cb35a36a1d87410cfbe0
Guilherme <gui.roussin@outlook.com>
2020-01-30 14:32:46 -0300

    Layout, HTML, CSS and SlickSlider Setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,37 @@
-let {series, src, dest, watch} = require('gulp')
+let { series, src, dest, watch } = require('gulp')
 let browserSync = require('browser-sync').create()
 let sass = require('gulp-sass')
 
-function _sass(cb){
-    return src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-    .pipe(sass())
-    .pipe(dest('src/css'))
-    .pipe(browserSync.stream())
+function _sass(cb) {
+    return src(
+        [
+            'node_modules/bootstrap/scss/bootstrap.scss',
+            'node_modules/slick-carousel/slick/slick.scss',
+            'src/scss/*.scss'
+        ]
+    )
+        .pipe(sass())
+        .pipe(dest('src/css'))
+        .pipe(browserSync.stream())
 
     cb()
 }
 
-function js(cb){
-    return src([
-        'node_modules/bootstrap/dist/js/bootstrap.min.js',
-        'node_modules/jquery/dist/jquery.min.js'
-    ])
-    .pipe(dest('src/js'))
-    .pipe(browserSync.stream())
+function js(cb) {
+    return src(
+        [
+            'node_modules/slick-carousel/slick/slick.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/jquery/dist/jquery.min.js'
+        ]
+    )
+        .pipe(dest('src/js'))
+        .pipe(browserSync.stream())
 
     cb()
 }
 
-function serve(cb){
+function serve(cb) {
     browserSync.init({
         server: './src'
     })
@@ -33,6 +42,13 @@ function serve(cb){
             'src/scss/*.scss'
         ]
         , series(_sass)
+    )
+
+    watch(
+        [
+            'src/js/js.js'
+        ]
+        , series(js)
     )
 
     watch('src/*.html').on('change', browserSync.reload)
